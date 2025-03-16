d2fc02d5514d007b8dac41b08dc3a8bffd737984
renavet <85550553+renavet@users.noreply.github.com>
2025-03-16 23:10:30 +0100

    Update search.js

--- _includes/search.js
+++ _includes/search.js
@@ -1,31 +1,35 @@
-// Funzione per cercare i file in base al nome
+// Funzione per cercare i file in tutta la directory
 function searchFiles() {
-    var input, filter, resultsDiv, fileNames;
+    var input, filter, resultsDiv;
     input = document.getElementById("search-input");
-    filter = input.value.toLowerCase(); // Ottieni il valore della ricerca
+    filter = input.value.toLowerCase();
     resultsDiv = document.getElementById("search-results");
 
-    // Aggiungi qui i nomi dei file (senza estensione)
-    fileNames = [
-        "about", // nome del file senza estensione
-        "contact",
-        "post-1",
-        "mio-post",
-        "articolo-xyz"
-        // Aggiungi qui altri file
+    // Aggiungi qui i nomi dei file (senza estensione, raccolti tramite Liquid)
+    var fileNames = [
+        {% for post in site.posts %}
+        "{{ post.url | remove: '.html' }}",  // Ottieni il percorso del file senza estensione
+        {% endfor %}
+        {% for page in site.pages %}
+        "{{ page.url | remove: '.html' }}",  // Ottieni il percorso della pagina senza estensione
+        {% endfor %}
+        {% for file in site.static_files %}
+        "{{ file.path | remove: '.html' }}", // Aggiungi anche file statici come immagini, CSS, JS
+        {% endfor %}
     ];
 
-    // Filtra i file in base alla ricerca
-    var filteredFiles = fileNames.filter(function (file) {
-        return file.toLowerCase().includes(filter); // Filtra per corrispondenza
+    // Filtraggio dei risultati
+    var filteredFiles = fileNames.filter(function(file) {
+        return file.toLowerCase().includes(filter); // Filtra per nome del file
     });
 
-    // Mostra i risultati
+    // Visualizzazione dei risultati
     if (filteredFiles.length > 0) {
-        resultsDiv.innerHTML = "<ul>" + filteredFiles.map(function (file) {
-            return "<li><a href='" + file + ".html'>" + file + "</a></li>";
+        resultsDiv.innerHTML = "<ul>" + filteredFiles.map(function(file) {
+            return "<li><a href='" + file + ".html'>" + file.split('/').pop().replace('-', ' ') + "</a></li>";
         }).join("") + "</ul>";
     } else {
         resultsDiv.innerHTML = "<p>Nessun risultato trovato</p>";
     }
 }
+
